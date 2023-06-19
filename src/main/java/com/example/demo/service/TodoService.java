b0a9818a7d0e6a4b0d1a895375f82e329d694b49
Shin Seo Yeon <ssy2013928@sookmyung.ac.kr>
2023-06-19 20:11:03 +0900

    백엔드 개발 18 @NamedQuery 사용하기

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -2,7 +2,6 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
 
@@ -10,14 +9,14 @@ import com.example.demo.persistence.TodoRepository;
 public class TodoService {
 	@Autowired
 	private TodoRepository repository;
-	
-	public String testService(){
-		// Todo Entity 생성
+
+	public String testService() {
+// Todo Entity 생성
 		TodoEntity entity = TodoEntity.builder().userId("Shin Seo Yeon").title("My first todo item").build();
-		// Todo Entity 저장
+//Todo Entity 저장
 		repository.save(entity);
-		// Todo Entity 검색
-		TodoEntity savedEntity = repository.findByUserId(entity.getUserId()).get(0);
+//Todo Entity 검색
+		TodoEntity savedEntity = repository.searchByUserId(entity.getUserId()).get(0);
 		return savedEntity.getUserId();
 	}
 }
